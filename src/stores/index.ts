4e30a7fe4f550c6e489d9d088516ca05af6d4728
R. Martinho Fernandes <rmf@rmf.io>
2025-09-17 02:21:53 +0200

    Initialize the project 🚀

---
+++ src/stores/index.ts
@@ -0,0 +1,14 @@
+import { defineStore } from '#q-app/wrappers';
+import { createPinia } from 'pinia';
+
+declare module 'pinia' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface PiniaCustomProperties {
+    // add your custom properties here, if any
+  }
+}
+
+export default defineStore(() => {
+  const pinia = createPinia();
+  return pinia;
+});
